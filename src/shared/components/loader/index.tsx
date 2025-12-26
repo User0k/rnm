2f23992f43686d10306aec20d942a57d2646ac33
Pavel Altov <pavel.altov@yahoo.com>
2025-12-26 12:46:06 +0300

    feat: добавил индикатор загрузки

---
+++ src/shared/components/loader/index.tsx
@@ -0,0 +1,26 @@
+import SpinnerLarge from '../../../assets/images/spinner_large.png';
+import SpinnerSmall from '../../../assets/images/spinner_large.png';
+import './loader.css';
+
+interface LoaderProps {
+  size?: 'large' | 'small';
+  label?: string;
+}
+
+function Loader({ size = 'large', label }: LoaderProps) {
+  const spinnerClassName = size === 'large' ? 'loader-large' : 'loader-small';
+  const spinnerSource = size === 'large' ? SpinnerLarge : SpinnerSmall;
+
+  return (
+    <div className='loader__wrapper'>
+      <img
+        src={spinnerSource}
+        className={spinnerClassName}
+        alt='Animated spinner as a portal from Rick and Morty'
+      />
+      {label && <h3>{label}</h3>}
+    </div>
+  );
+}
+
+export default Loader;
