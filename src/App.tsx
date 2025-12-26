38dabb5756a421fa149c087123db1aa33249860b
Pavel Altov <pavel.altov@yahoo.com>
2025-12-26 19:28:38 +0300

    feat: добавил базовый роутинг, страницу информации о персонаже

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import CharacterInfo from './pages/character-info';
 import CharacterList from './pages/character-list';
+import { Routes, Route } from 'react-router';
 
 export default function App() {
-  return <CharacterList />;
+  return (
+    <Routes>
+      <Route
+        path='/'
+        element={<CharacterList />}
+      />
+      <Route
+        path='/character/:id'
+        element={<CharacterInfo />}
+      />
+    </Routes>
+  );
 }
---
+++ src/pages/character-info/index.tsx
@@ -0,0 +1,34 @@
+import Layout from '../../shared/components/layout';
+import ArrowIcon from '../../assets/icons/arrow_back.svg?react';
+import Loader from '../../shared/components/loader';
+import { useNavigate } from 'react-router';
+import { useState } from 'react';
+import './character-info.css';
+
+function CharacterInfo() {
+  const [isLoading] = useState(true);
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    navigate('/');
+  };
+
+  return (
+    <Layout>
+      <div className='container-medium character-info__button-wrapper'>
+        <button
+          className='character-info__button-back'
+          onClick={handleGoBack}
+        >
+          <ArrowIcon />
+          <h3>go back</h3>
+        </button>
+      </div>
+      <div className='container-medium'>
+        {isLoading && <Loader label='Loading character card...' />}
+      </div>
+    </Layout>
+  );
+}
+
+export default CharacterInfo;
